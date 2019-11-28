3396aa6e127c33ccb80429bdc63ecad3a618185c
samadhan patil <samadhan.patil.sp@gmail.com>
2019-11-28 15:59:32 +0530

    add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,43 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { RegisterComponent } from './register/register.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { EditComponent } from './edit/edit.component';
+import { DeleteComponent } from './delete/delete.component';
+import {RouterModule} from '@angular/router';
+import {HttpClientModule, HttpClient} from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
+    RegisterComponent,
+    NotFoundComponent,
+    EditComponent,
+    DeleteComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      {path : "", component : HomeComponent},
+      {path : "home", component : HomeComponent},
+      {path : "register", component : RegisterComponent},
+      {path : "about", component :AboutComponent},
+      {path : "contact", component : ContactComponent},
+      {path : "edit/:No", component : EditComponent},
+      {path : "delete/:No", component : DeleteComponent},
+      {path : "**", component : NotFoundComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
